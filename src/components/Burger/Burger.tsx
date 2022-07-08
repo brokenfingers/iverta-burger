2338fccfee6b303e9efdf10b21be14f735a23ac2
brokenfingers <buvotrys@gmail.com>
2022-07-08 12:56:27 +0300

    dynamic burger ingredients

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -9,21 +9,39 @@
 
 // export default Burger
 
+import { number } from 'prop-types';
 import classes from './Burger.module.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 interface Props {
-    ingredients: {}
+    ingredients: Ingredients
+}
+
+interface Ingredients {
+    // salad?: number;
+    // meat?: number;
+    // bacon?: number;
+    // cheese?: number
+    [key:string]:number
+
 }
 
 
 const Burger = (props:Props) => {
+    const ingredients:Ingredients = props.ingredients
+    const transformedIngredients = Object.keys(props.ingredients).map(ingrKey => {
+        return [...Array(ingredients[ingrKey])].map((_, i) => {
+            return <BurgerIngredients key={ingrKey+i} type={ingrKey}/>
+        })
+    } )
+
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top"/>
-            <BurgerIngredients type="cheese"/>
-            <BurgerIngredients type="meat"/>
+                {
+                    transformedIngredients
+                }
             <BurgerIngredients type="bread-bottom"/>
         </div>
     );
